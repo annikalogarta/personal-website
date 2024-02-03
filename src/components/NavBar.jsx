b45e4b32aa1827529843e3bbc71d61c94fcf1a83
Annika <67515092+annikalogarta@users.noreply.github.com>
2024-02-02 21:36:20 -0800

    Modified navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import Icon from "../assets/logo.png"
+import Pdf from "../assets/AnnikaLogarta_Resume_Test.pdf"
+
+export default function NavBar() {
+    
+    return (
+        <>
+        <div className="fixed top-0 left-0 w-screen h-15 m-0 flex text-white bg-[#130C1E] justify-end shadow-md">
+            <a href="#top" className="float-left block text-center px-7 py-5 mr-auto"><img src={ Icon } alt="Logo"  className="h-6"/></a>
+            <nav>
+                <ul className="list-none overflow-hidden mr-8">
+                    <div className="">
+                    
+                    <li className="float-right block text-center px-7 py-5 hover:text-orange duration-300"><a href={ Pdf } target="_blank" rel="noopener noreferrer">Resume</a></li>
+                    <li className="float-right block text-center px-7 py-5 hover:text-orange duration-300"><a href="#projects">Projects</a></li>
+                    <li className="float-right block text-center px-7 py-5 hover:text-orange duration-300"><a href="#about">About</a></li>
+                    </div>
+                </ul>
+            </nav>
+            
+        
+        </div></>
+    )
+};
