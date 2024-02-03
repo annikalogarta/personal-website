712b63af95a60275603944764724592ec858852e
Annika <67515092+annikalogarta@users.noreply.github.com>
2024-02-02 22:07:11 -0800

    debugging vercel deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import NavBar from "/components/NavBar"
-import Home from "/pages/Home"
-import About from "/pages/About"
-import Projects from "/pages/Projects"
-import Footer from "/pages/Footer"
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Footer from "./pages/Footer"
 
 function App() {
 
